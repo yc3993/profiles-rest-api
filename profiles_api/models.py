db6ab90eb4bd1431ccf73fd6eb1441e8368c6855
yc3993 <90934485+yc3993@users.noreply.github.com>
2022-01-04 11:56:05 -0500

    Enable django admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,7 +22,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """create and save a new superuser"""
-        user = self.createuser(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
