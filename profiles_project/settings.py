0d357362ee93f5325971b14fe96fd7dedea5029f
yc3993 <90934485+yc3993@users.noreply.github.com>
2022-01-04 20:45:37 -0500

    Configured for deployment.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ogfa1rsp&h^$#o5gksqx^qvbuf&g7dh*4hd@ywvt3$bm!+-sq)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
+
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+STATIC_ROOT = 'static/'
